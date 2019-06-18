0c38bc691c6de39d622f0ccd01b72e04e4f098cd
Brian Van Klaveren <bvan@slac.stanford.edu>
2019-06-18 11:34:27 -0700

    Use printf instead of echo

--- kube/template/init.sh
+++ kube/template/init.sh
@@ -20,19 +20,19 @@ read OAUTH2_PROXY_CLIENT_ID
 echo "CILogon Client Secret:"
 read OAUTH2_PROXY_CLIENT_SECRET
 
-OAUTH2_PROXY_CLIENT_SECRET_B64=$(echo $OAUTH2_PROXY_CLIENT_SECRET | $b64)
+OAUTH2_PROXY_CLIENT_SECRET_B64=$(printf -- "$OAUTH2_PROXY_CLIENT_SECRET" | $b64)
 
 OAUTH2_PROXY_COOKIE_SECRET=$(dd if=/dev/urandom bs=32 count=1 2> /dev/null | $b64 | sed 's/+/-/g;s/\//_/g')
-OAUTH2_PROXY_COOKIE_SECRET_B64=$(echo $OAUTH2_PROXY_COOKIE_SECRET | $b64)
+OAUTH2_PROXY_COOKIE_SECRET_B64=$(printf -- "$OAUTH2_PROXY_COOKIE_SECRET" | $b64)
 
 echo "Generating Issuer Keypair... private.pem, public.pem"
 openssl genrsa -out private.pem 2048 2> /dev/null
 openssl rsa -in private.pem -outform PEM -pubout -out public.pem
 modulus_hex=$(openssl rsa -pubin -inform PEM -modulus -noout -in public.pem | sed 's/Modulus=//')
-modulus_urlsafe_b64=$(echo $modulus_hex | xxd -r -p | $b64 | sed 's/+/-/g;s/\//_/g')
+modulus_urlsafe_b64=$(printf -- "$modulus_hex" | xxd -r -p | $b64 | sed 's/+/-/g;s/\//_/g')
 
 ISSUER_PRIVATE_KEY=$(cat private.pem)
-ISSUER_PRIVATE_KEY_INDENT_10=$(echo "$ISSUER_PRIVATE_KEY" | sed 's/^/          /')
+ISSUER_PRIVATE_KEY_INDENT_10=$(printf -- "$ISSUER_PRIVATE_KEY" | sed 's/^/          /')
 JWKS_N=$modulus_urlsafe_b64
 
 AUTHORIZER_FLASK_SECRET=$(dd if=/dev/urandom bs=32 count=1 2> /dev/null | $b64)
@@ -45,7 +45,7 @@ cat <<EOF > ${NAMESPACE}/data.yml
 AUTHORIZER_FLASK_SECRET: ${AUTHORIZER_FLASK_SECRET} 
 HOSTNAME: ${HOSTNAME} 
 ISSUER_PRIVATE_KEY_INDENT_10: |2
-$(echo "$ISSUER_PRIVATE_KEY_INDENT_10" | sed 's/^/  /')
+$(printf -- "$ISSUER_PRIVATE_KEY_INDENT_10" | sed 's/^/  /')
 JWKS_N: ${JWKS_N} 
 NAMESPACE: ${NAMESPACE} 
 OAUTH2_PROXY_CLIENT_ID: ${OAUTH2_PROXY_CLIENT_ID} 
@@ -61,7 +61,7 @@ j2 --version > /dev/null
 has_j2_rc=$?
 
 if [[ $has_j2_rc -ne 0 ]]; then
-    volumes=$(for dir in configmap deployment ing secret svc ${NAMESPACE}; do echo "-v `pwd`/$dir:/$dir"; done)
+    volumes=$(for dir in configmap deployment ing secret svc ${NAMESPACE}; do echo -n "-v `pwd`/$dir:/$dir"; done)
     j2="docker run $volumes -w / danielpanzella/j2cli"
 fi
 
