5f30348a162b9a0619aab93521e529477ca3ac78
Russ Allbery <rra@lsst.org>
2020-03-06 12:36:49 -0800

    [DM-23646] Switch to the new multi-stage Docker build
    Update the Dockerfile to the roundtable_aiohttp_bot version, with
    some modifications for uwsgi.
    
    Remove the old docker deploy script, which is now redundant with
    GitHub actions.  Add an install-base-packages.sh script and install
    the requirements for uwsgi now that we're using a slim image.

---
+++ scripts/install-base-packages.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script updates packages in the base Docker image that's used by both the
+# build and runtime images, and gives us a place to install additional
+# system-level packages with apt-get.
+#
+# Based on the blog post:
+# https://pythonspeed.com/articles/system-packages-docker/
+
+# Bash "strict mode", to help catch problems and bugs in the shell
+# script. Every bash script you write should include this. See
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/ for
+# details.
+set -euo pipefail
+
+# Tell apt-get we're never going to be able to give manual
+# feedback:
+export DEBIAN_FRONTEND=noninteractive
+
+# Update the package listing, so we know what packages exist:
+apt-get update
+
+# Install security updates:
+apt-get -y upgrade
+
+# git is required by setuptools-scm.  build-essential and libssl-dev are
+# required by uwsgi
+apt-get -y install --no-install-recommends build-essential git libssl-dev
+
+# Delete cached files we don't need anymore:
+apt-get clean
+rm -rf /var/lib/apt/lists/*
