18ed1273fc470ed647aab8f68eba7757539b2b84
Russ Allbery <rra@lsst.org>
2022-12-09 11:43:19 -0800

    Move test configuration files
    There is about to be more test data, so rather than littering the
    top level of the test directory with lots of data directories,
    move the config files into a data subdirectory that can be reused
    for other things.

--- tests/config_test.py
+++ tests/config_test.py
@@ -12,7 +12,7 @@ from gafaelfawr.config import Config, Settings
 from gafaelfawr.exceptions import InvalidTokenError
 from gafaelfawr.models.token import Token
 
-from .support.config import build_config
+from .support.config import build_config, config_path
 
 
 def parse_config(path: Path, fix_token: bool = False) -> None:
@@ -39,52 +39,46 @@ def parse_config(path: Path, fix_token: bool = False) -> None:
 def test_config_examples() -> None:
     """Check that all of the example configuration files validate."""
     examples_path = Path(__file__).parent.parent / "examples"
-    for config_path in examples_path.iterdir():
-        if config_path.name.endswith(".yaml"):
-            parse_config(config_path, fix_token=True)
+    for path in examples_path.iterdir():
+        if path.name.endswith(".yaml"):
+            parse_config(path, fix_token=True)
 
 
 def test_config_no_provider() -> None:
-    config_path = Path(__file__).parent / "config" / "no-provider.yaml"
     with pytest.raises(ValidationError):
-        parse_config(config_path)
+        parse_config(config_path("no-provider.yaml"))
 
 
 def test_config_both_providers() -> None:
-    config_path = Path(__file__).parent / "config" / "both-providers.yaml"
     with pytest.raises(ValidationError):
-        parse_config(config_path)
+        parse_config(config_path("both-providers.yaml"))
 
 
 def test_config_invalid_admin() -> None:
-    config_path = Path(__file__).parent / "config" / "bad-admin.yaml"
     with pytest.raises(ValidationError):
-        parse_config(config_path)
+        parse_config(config_path("bad-admin.yaml"))
 
 
 def test_config_invalid_loglevel() -> None:
-    config_path = Path(__file__).parent / "config" / "bad-loglevel.yaml"
     with pytest.raises(ValidationError):
-        parse_config(config_path)
+        parse_config(config_path("bad-loglevel.yaml"))
 
 
 def test_config_invalid_scope() -> None:
-    config_path = Path(__file__).parent / "config" / "bad-scope.yaml"
     with pytest.raises(ValidationError):
-        parse_config(config_path)
+        parse_config(config_path("bad-scope.yaml"))
 
 
 def test_config_missing_scope() -> None:
-    config_path = Path(__file__).parent / "config" / "missing-scope.yaml"
     with pytest.raises(ValidationError):
-        parse_config(config_path)
+        parse_config(config_path("missing-scope.yaml"))
 
 
 def test_config_invalid_token(tmp_path: Path) -> None:
     bootstrap_token_file = tmp_path / "bootstrap-bad"
     bootstrap_token_file.write_bytes(b"bad-token")
-    config_path = build_config(
+    path = build_config(
         tmp_path, "bad-token", bootstrap_token_file=str(bootstrap_token_file)
     )
     with pytest.raises(InvalidTokenError):
-        Config.from_file(config_path)
+        Config.from_file(path)
