892982ad1a3486f4ee9c9f38023812e0dc35b475
Russ Allbery <rra@lsst.org>
2022-12-09 18:02:49 -0800

    Rework GafaelfawrServiceToken operator tests
    Use a data-driven model where the input and output YAML are files
    in the tests directory instead of hard-coded as Python dicts in
    the test suite.  Add a bunch of new helper functions to make it
    easier to share code between tests.

---
+++ tests/data/kubernetes/README.md
@@ -0,0 +1,24 @@
+# Kubernetes operator test data
+
+The files in this directory are used in tests for Kubernetes operators.
+
+The `input` subdirectory contains custom resources that the operator is expected to act on.
+The `output` subdirectory contains the results.
+File names should match between the directories: a file in `input` should create the resources contained in the corresponding file in `output`.
+
+There may be files in `input` with no corresponding file in `output` to test error cases.
+
+All files are in templated YAML.
+Templating is done through the regular Python `format` syntax.
+The following variables will be expanded before the files are used:
+
+`namespace`
+    The namespace used by the test.
+    These namespaces have random names and are created by a fixture before each Kubernetes operator test.
+
+The following variables are expanded only in output files:
+
+`any`
+    Becomes `unittest.mock.ANY`.
+
+Both the `input` and `output` files may contain multiple resources using the normal YAML `---` document separator.
--- tests/support/kubernetes.py
+++ tests/support/kubernetes.py
@@ -4,9 +4,12 @@ from __future__ import annotations
 
 import asyncio
 import os
-from collections.abc import AsyncIterator, Iterator
+from collections.abc import AsyncIterator, Iterable, Iterator, Mapping
 from contextlib import asynccontextmanager, contextmanager
+from datetime import datetime, timedelta
 from pathlib import Path
+from typing import Any
+from unittest.mock import ANY
 
 import pytest
 import yaml
@@ -20,7 +23,12 @@ from kubernetes_asyncio.client import (
     V1ObjectMeta,
 )
 
+from gafaelfawr.models.kubernetes import KubernetesResourceStatus, StatusReason
+from gafaelfawr.util import current_datetime
+
 __all__ = [
+    "assert_resources_match",
+    "create_custom_resources",
     "install_crds",
     "operator_running",
     "requires_kubernetes",
@@ -28,12 +36,199 @@ __all__ = [
     "temporary_namespace",
 ]
 
+_PLURALS = {
+    "GafaelfawrServiceToken": "gafaelfawrservicetokens",
+}
+"""Mapping of kinds to plurals for the custom object API."""
+
 
 requires_kubernetes = pytest.mark.skipif(
     os.getenv("TEST_KUBERNETES") is None,
     reason="Install minikube and set TEST_KUBERNETES to run test",
 )
 """Decorator to mark tests that should only be run if Kubernetes is enabled."""
+
+
+def _replace_any(data: Any) -> Any:
+    """Replace ``<ANY>`` strings with `~unittest.mock.ANY`."""
+    if isinstance(data, str):
+        return ANY if data == "<ANY>" else data
+    elif isinstance(data, Mapping):
+        return {k: _replace_any(v) for k, v in data.items()}
+    elif isinstance(data, Iterable):
+        return [_replace_any(e) for e in data]
+    else:
+        return data
+
+
+def operator_test_input(filename: str, namespace: str) -> list[dict[str, Any]]:
+    """Read input for a Kubernetes operator test from a file.
+
+    Parameters
+    ----------
+    filename
+        Name of file in ``tests/data/kubernetes/input`` containing the
+        resources in YAML format.
+    namespace
+        Namespace to use for the objects.
+
+    Returns
+    -------
+    list of dict
+        The custom resources.
+    """
+    path = (
+        Path(__file__).parent.parent
+        / "data"
+        / "kubernetes"
+        / "input"
+        / (filename + ".yaml")
+    )
+    resources = path.read_text().format(namespace=namespace)
+    return list(yaml.safe_load_all(resources))
+
+
+def operator_test_output(
+    filename: str, namespace: str
+) -> list[dict[str, Any]]:
+    """Read output for a Kubernetes operator test from a file.
+
+    Parameters
+    ----------
+    filename
+        Name of file in ``tests/data/kubernetes/output`` containing the
+        resources in YAML format.
+    namespace
+        Namespace to use for the objects.
+
+    Returns
+    -------
+    list of dict
+        The custom resources.
+    """
+    path = (
+        Path(__file__).parent.parent
+        / "data"
+        / "kubernetes"
+        / "output"
+        / (filename + ".yaml")
+    )
+    resources = path.read_text().format(namespace=namespace, any="<ANY>")
+    return _replace_any(yaml.safe_load_all(resources))
+
+
+async def create_custom_resources(
+    api_client: ApiClient, resources: list[dict[str, Any]]
+) -> None:
+    """Create test resources.
+
+    Parameters
+    ----------
+    api_client
+        Kubernetes client.
+    resources
+        The resources to create.
+    """
+    custom_api = CustomObjectsApi(api_client)
+    for resource in resources:
+        await custom_api.create_namespaced_custom_object(
+            "gafaelfawr.lsst.io",
+            "v1alpha1",
+            resource["metadata"]["namespace"],
+            _PLURALS[resource["kind"]],
+            resource,
+        )
+
+
+async def assert_custom_resource_status_is(
+    api_client: ApiClient,
+    resource: dict[str, Any],
+    status: KubernetesResourceStatus,
+) -> None:
+    """Asserts that the status of a custom object matches.
+
+    Parameters
+    ----------
+    api_client
+        Kubernetes client.
+    resource
+        The custom resource to check. It will be retrieved from Kubernetes
+        again to get its updated status.
+    status
+        The expected status.
+    """
+    custom_api = CustomObjectsApi(api_client)
+    seen = await custom_api.get_namespaced_custom_object(
+        "gafaelfawr.lsst.io",
+        "v1alpha1",
+        resource["metadata"]["namespace"],
+        _PLURALS[resource["kind"]],
+        resource["metadata"]["name"],
+    )
+    if status.reason == StatusReason.Failed:
+        status_value = "False"
+    else:
+        status_value = "True"
+    assert seen["status"] == {
+        "create": {
+            "lastTransitionTime": ANY,
+            "message": status.message,
+            "observedGeneration": seen["metadata"]["generation"],
+            "reason": status.reason.value,
+            "status": status_value,
+            "type": "ResourceCreated",
+        }
+    }
+    transition_str = seen["status"]["create"]["lastTransitionTime"]
+    assert transition_str.endswith("Z")
+
+    # Python 3.11 can parse the string as-is, but Python 3.10 and earlier
+    # doesn't like the Z.
+    transition = datetime.fromisoformat(transition_str.replace("Z", "+00:00"))
+
+    now = current_datetime()
+    assert now - timedelta(seconds=30) <= transition <= now
+
+
+async def assert_resources_match(
+    api_client: ApiClient, resources: list[dict[str, Any]]
+) -> None:
+    """Asserts that output resources match the provided file match.
+
+    Also verifies there are no output resources of the indicated type in the
+    given namespace, except possibly for ones starting with ``default`` (to
+    allow for the ``Secret`` resources holding tokens for the default service
+    account).
+
+    Parameters
+    ----------
+    api_client
+        Kubernetes client.
+    resources
+        The resources to compare. All resources must have the same ``kind``.
+    """
+    kind = resources[0]["kind"]
+
+    checked = set()
+    for expected in resources:
+        name = expected["metadata"]["name"]
+        namespace = expected["metadata"]["namespace"]
+        if kind == "Secret":
+            core_api = CoreV1Api(api_client)
+            seen = await core_api.read_namespaced_secret(name, namespace)
+        else:
+            assert False, f"Unknown object kind {kind}"
+        assert api_client.sanitize_for_serialization(seen) == expected
+        checked.add(name)
+
+    if kind == "Secret":
+        core_api = CoreV1Api(api_client)
+        secrets = await core_api.list_namespaced_secret(namespace)
+        for secret in secrets.items:
+            name = secret.metadata.name
+            assert name in checked, f"Unexpected secret {name}"
+    else:
+        assert False, f"Unknown object kind {kind}"
 
 
 async def install_crds(api_client: ApiClient) -> None:
@@ -127,7 +322,8 @@ async def temporary_namespace(api_client: ApiClient) -> AsyncIterator[str]:
     yield namespace
 
     # Remove finalizers from all of our custom objects so that they can be
-    # deleted without Kopf running.
+    # deleted without Kopf running.  This only needs to be done for objects
+    # that have timers; create and modify hooks don't create finalizers.
     service_tokens = await custom_api.list_namespaced_custom_object(
         "gafaelfawr.lsst.io",
         "v1alpha1",
@@ -144,4 +340,5 @@ async def temporary_namespace(api_client: ApiClient) -> AsyncIterator[str]:
                 service_token["metadata"]["name"],
                 [{"op": "remove", "path": "/metadata/finalizers"}],
             )
+
     await core_api.delete_namespace(namespace)
