92e0c8a48483f51aa7ad5ea1d325566ffa0f8ced
Russ Allbery <rra@lsst.org>
2024-02-12 16:37:56 -0800

    Do not run database initialization on startup
    It's not generally safe to run Alembic upgrades on startup since
    we may have multiple replicas, and it's not safe to run
    gafaelfawr init before Alembic because it will stap the database
    with the current version even though it didn't make all the required
    changes.
    
    Drop both from the startup script for now. We will need to find a
    better way of doing both database initialization and schema changes,
    probably using Helm hooks.

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,14 +4,6 @@
 
 set -eu
 
-# Always initialize the database if needed. This shouldn't require LDAP access
-# and thus isn't run with Kerberos tickets.
-gafaelfawr init
-
-# Perform any Alembic migrations that are needed.
-cd /app
-alembic upgrade head
-
 # Start the server under k5start if Kerberos is configured.
 cmd="uvicorn --factory gafaelfawr.main:create_app --host 0.0.0.0 --port 8080"
 if [ -f "/etc/krb5.conf" ] && [ -f "/etc/krb5.keytab" ]; then
