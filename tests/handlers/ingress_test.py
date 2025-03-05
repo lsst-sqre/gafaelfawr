4f972019a149513a448599270a35d5d685ab9b3c
Russ Allbery <rra@lsst.org>
2025-03-04 18:42:00 -0800

    Add support for per-user domain authorization
    Add a new setting to `GafaelfawrIngress` named `config.userDomain`
    that, if set to true, requires that the last component of the
    request URL match the authenticated username. This is intended for
    use with per-user subdomains, such as Jupyter labs with JavaScript
    origin isolation. In this case, as an additional sanity check, also
    require that the hostname in the request be a subdomain of Gafaelfawr's
    base domain.

--- tests/handlers/ingress_test.py
+++ tests/handlers/ingress_test.py
@@ -1182,3 +1182,50 @@ async def test_allow_cookies(client: AsyncClient, factory: Factory) -> None:
     assert r.status_code == 401
     authenticate = parse_www_authenticate(r.headers["WWW-Authenticate"])
     assert authenticate.auth_type == AuthType.Bearer
+
+
+@pytest.mark.asyncio
+async def test_user_domain(client: AsyncClient, factory: Factory) -> None:
+    config = await reconfigure("github-subdomain")
+    token_data = await create_session_token(
+        factory, group_names=["admin"], scopes={"read:all"}
+    )
+
+    # Successful authentications where the username matches the hostname.
+    for hostname in (
+        f"{token_data.username}.{config.base_hostname}",
+        f"{token_data.username}.nb.{config.base_hostname}",
+    ):
+        r = await client.get(
+            "/ingress/auth",
+            params={"scope": "read:all", "user_domain": "true"},
+            headers={
+                "Authorization": f"Bearer {token_data.token}",
+                "X-Original-URL": f"https://{hostname}/foo",
+            },
+        )
+        assert r.status_code == 200, hostname
+        assert r.headers["X-Auth-Request-User"] == token_data.username
+
+    # Failed authentications where either the username doesn't match or the
+    # hostname looks broken.
+    for hostname in (
+        config.base_hostname,
+        token_data.username,
+        f"{token_data.username}.example.org",
+        f"nb.{token_data.username}.{config.base_hostname}",
+        f"{token_data.username}.{config.base_hostname}.com",
+    ):
+        r = await client.get(
+            "/ingress/auth",
+            params={"scope": "read:all", "user_domain": "true"},
+            headers={
+                "Authorization": f"Bearer {token_data.token}",
+                "X-Original-URL": f"https://{hostname}/foo",
+            },
+        )
+        assert r.status_code == 403, hostname
+        authenticate = parse_www_authenticate(r.headers["WWW-Authenticate"])
+        assert isinstance(authenticate, AuthErrorChallenge)
+        assert authenticate.auth_type == AuthType.Bearer
+        assert authenticate.error == AuthError.insufficient_scope
